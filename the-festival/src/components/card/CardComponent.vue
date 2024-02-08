d0db94f3ca6406109374906778dd962d28486c49
KoenMW <89910070+KoenMW@users.noreply.github.com>
2024-02-08 13:33:22 +0100

    initial

---
+++ the-festival/src/components/card/CardComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="card">
+        <img :src="image" :alt="title" />
+        <h2>{{ title }}</h2>
+        <p>{{ description }}</p>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        image: String,
+        title: String,
+        description: String
+    }
+}
+</script>
+
+<style scoped>
+
+@import './card.scss';
+</style>

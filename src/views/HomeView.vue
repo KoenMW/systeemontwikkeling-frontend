e48f2bfd72b67e362b68a9b680b714fa04a3f9d4
KoenMW <89910070+KoenMW@users.noreply.github.com>
2024-02-08 14:02:02 +0100

    Refactor card component styles and add card to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <script setup>
+import Card from '@/components/card/CardComponent.vue';
 </script>
 
 <template>
   <main>
+    <Card
+      image="https://via.placeholder.com/300"
+      title="Card Title"
+      description="Card Description"
+    />
   </main>
 </template>

14a9dfef35035b3e7f766816370e1e635bdc1c46
KoenMW <89910070+KoenMW@users.noreply.github.com>
2024-02-10 20:28:36 +0100

    Add event classes to CardComponent and update variables.scss

--- src/components/card/CardComponent.vue
+++ src/components/card/CardComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="card">
+    <section :class="['card', event ?? '']">
         <img :src="image" :alt="title" />
         <h2>{{ title }}</h2>
         <p>{{ description }}</p>
@@ -7,13 +7,30 @@
 </template>
 
 <script>
+
 export default {
     props: {
-        image: String,
-        title: String,
-        description: String
+        image: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        event: {
+            type: String,
+            validator: (value) => {
+                return ['history', 'jazz', 'music'].includes(value);
+            },            
+        },
     }
 }
+
 </script>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,12 +8,20 @@ import Card from '@/components/card/CardComponent.vue';
       image="https://via.placeholder.com/300"
       title="Card Title"
       description="Card Description"
+      event="history"
     />
 
     <Card
       image="https://via.placeholder.com/300"
       title="Card Title 2"
       description="Card Description 2"
+      event="music"
+    />
+    <Card
+      image="https://via.placeholder.com/300"
+      title="Card Title 3"
+      description="Card Description 3"
+      event="jazz"
     />
   </main>
 </template>

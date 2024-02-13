5c4aac2e72dc6b283ba49da70939b9f9bea2f08c
KoenMW <89910070+KoenMW@users.noreply.github.com>
2024-02-13 08:59:30 +0100

    added employee view with qr reader

---
+++ src/views/Employee/EmployeeView.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <QrcodeStream @detect="onDetect"></QrcodeStream>
+
+</template>
+
+
+<script>
+import { QrcodeStream } from 'vue-qrcode-reader'
+
+export default {
+    props: {
+
+    },
+    components: {
+        QrcodeStream
+    },
+    methods: {
+         onDetect (detectedCodes) {
+            console.log('codes: ', detectedCodes);
+         }
+    }
+}
+
+</script>

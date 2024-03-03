d693487a7e8bbc893ebd3bb0d7a4b7e8f805a88e
NickS721 <113018071+NickS721@users.noreply.github.com>
2024-03-03 19:50:26 +0100

    header changes

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -1,43 +1,36 @@
-<script setup>
-  import { ref } from 'vue';
-  import { RouterLink } from 'vue-router';
-
-  const showUserDropdown = ref(false);
-</script>
-
 <template>
-  <div>
-    <RouterLink to="/user" class="header-link user" @click.prevent="showUserDropdown = !showUserDropdown">
-      <div class="header-image user"></div>
+  <div class="header-container">
+    <RouterLink to="/user" class="header-link">
+      <div class="header-image user">User</div>
     </RouterLink>
-    <div v-if="showUserDropdown" class="user-dropdown">
-      <RouterLink to="/login">Login</RouterLink>
-      <RouterLink to="/register">Register</RouterLink>
-    </div>
+    <RouterLink to="/dance" class="header-link">
+      <div class="header-image dance">Dance</div>
+    </RouterLink>
+    <RouterLink to="/jazz" class="header-link">
+      <div class="header-image jazz">Jazz</div>
+    </RouterLink>
+    <RouterLink to="/yummy" class="header-link">
+      <div class="header-image yummy">Yummy</div>
+    </RouterLink>
+    <RouterLink to="/" class="header-link">
+      <div class="header-image home">Home</div>
+    </RouterLink>
+    <RouterLink to="/history" class="header-link">
+      <div class="header-image history">History</div>
+    </RouterLink>
+    <RouterLink to="/shop" class="header-link">
+      <div class="header-image shop">Shop</div>
+    </RouterLink>
+    <RouterLink to="/language" class="header-link">
+      <div class="header-image language">Language</div>
+    </RouterLink>
   </div>
-  <RouterLink to="/dance" class="header-link">
-    <div class="header-image dance">Dance</div>
-  </RouterLink>
-  <RouterLink to="/jazz" class="header-link">
-    <div class="header-image jazz">Jazz</div>
-  </RouterLink>
-  <RouterLink to="/yummy" class="header-link">
-    <div class="header-image yummy">Yummy</div>
-  </RouterLink>
-  <RouterLink to="/" class="header-link">
-    <div class="header-image home">Home</div>
-  </RouterLink>
-  <RouterLink to="/history" class="header-link">
-    <div class="header-image history">History</div>
-  </RouterLink>
-  <RouterLink to="/shop" class="header-link shop">
-    <div class="header-image shop"></div>
-  </RouterLink>
-  <RouterLink to="/language" class="header-link">
-    <div class="header-image language"></div>
-  </RouterLink>
 </template>
 
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
 <style scoped>
-  @import './header.scss';
+@import './header.scss';
 </style>

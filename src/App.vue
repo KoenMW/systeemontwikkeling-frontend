6dd9ddc31319fc14c923c62d27e3a670552c9d15
KoenMW <89910070+KoenMW@users.noreply.github.com>
2024-03-04 19:20:14 +0100

    deleted unused code

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router';
 
 import CardComponent from './components/card/CardComponent.vue';
 import HeaderComponent from './components/header/HeaderComponent.vue';

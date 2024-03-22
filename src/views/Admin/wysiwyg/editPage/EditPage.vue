ef6971f942f4bddd8c72ce8985f2c4d6d1d7ba83
KoenMW <89910070+KoenMW@users.noreply.github.com>
2024-03-22 22:23:37 +0100

    Update margin in admin.scss and add banner and card components in EditPage.vue

--- src/views/Admin/wysiwyg/editPage/EditPage.vue
+++ src/views/Admin/wysiwyg/editPage/EditPage.vue
@@ -1,17 +1,34 @@
 <template>
-    <section class="">
-        test
+  <section class="editSection">
+    <bannerComponent :title="pageData.name" :description="pageData.intro" :image="pageData.picture" />
+
+    <section id="" v-if="infoText.length > 0">
+        <div v-for="info in infoText" :key="info.id">
+            <h2>{{info.title}}</h2>
+            <p>{{info.content}}</p>
+        </div>
     </section>
+
+    <section id="cards">
+        <CardComponent v-for="card in pageData.cards" :key="card.name" :title="card.title" :description="card.text" :image="card.picture"/>
+    </section>
+  </section>
+
 </template>
 
 
 <script>
 import axios from '../../../../axios-auth';
+import bannerComponent from '@/components/banner/bannerComponent.vue';
+import CardComponent from '@/components/card/CardComponent.vue';
 
 export default {
   data() {
-    return {
-    };
+        return {
+            pageData: {},
+            events: [],
+            infoText: []
+        }
   },
   mounted() {
     console.log('mounted');
@@ -19,13 +36,28 @@ export default {
     const pageName = this.$route.params.id;
     axios.get(`/pages/${pageName}`)
       .then(response => {
-        console.log(response.data);
+        this.pageData = response.data;
+        console.log(this.pageData.name);
+        this.pageData.infoText.forEach((info) => {
+          this.infoText.push(info);
+        })
+        this.pageData.events.forEach((event) => {
+          this.events.push(event);
+        })
       })
       .catch(error => {
         console.log(error);
       });
   },
+  components: {
+    bannerComponent,
+    CardComponent
+  }
 
 };
 
 </script>
+
+<style>
+@import 'EditPage.scss'
+</style>

619286000d40c9c734f8662af850739a2dd470a9
647825 <89908526+647825@users.noreply.github.com>
2024-03-24 14:57:10 +0100

    show correcet role

--- src/views/Admin/UsersView.vue
+++ src/views/Admin/UsersView.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="user-management">
+  <div class="management">
     <button @click="navigateTo('signup', { adminAdd: true })">Add User</button>
     <div class="filters">
-      <input v-model="searchQuery" @input="fetchUsers" placeholder="Search users..." class="search-input">
+      <input
+        v-model="searchQuery"
+        @input="fetchUsers"
+        placeholder="Search users..."
+        class="search-input"
+      />
       <select v-model="filterRole" @change="fetchUsers" class="role-select">
         <option value="">All Roles</option>
         <option value="0">User</option>
@@ -15,7 +20,7 @@
       </select>
     </div>
 
-    <table class="users-table">
+    <table>
       <thead>
         <tr>
           <th @click="setSortField('email')">Email</th>
@@ -26,21 +31,20 @@
       <tbody>
         <tr v-for="user in users" :key="user.id">
           <td>{{ user.email }}</td>
-          <td>{{ user.role }}</td>
+          <td>{{ getRoleName(user.role) }}</td>
           <td>{{ user.createDate }}</td>
           <td>
             <button @click="editUser(user.id)">Edit</button>
             <button @click="deleteUser(user.id)">Delete</button>
           </td>
         </tr>
-
       </tbody>
     </table>
   </div>
 </template>
 
 <script>
-import axios from '../../axios-auth.js';
+import axios from '../../axios-auth.js'
 
 export default {
   data() {
@@ -50,48 +54,57 @@ export default {
       filterRole: '',
       sortField: 'email',
       sortOrder: 'ASC',
-      isAdminAdd: this.$route.query.adminAdd,
-    };
+      isAdminAdd: this.$route.query.adminAdd
+    }
   },
 
   methods: {
+    getRoleName(roleNumber) {
+      const roleNames = {
+        0: 'User',
+        1: 'Employee',
+        2: 'Admin'
+      }
+      return roleNames[roleNumber] || 'Unknown'
+    },
     fetchUsers() {
+      const params = {
+        searchEmail: this.searchQuery,
+        sortByCreateDate: this.sortOrder,
+        sortField: this.sortField
+      }
+      if (this.filterRole !== '') {
+        params.filterRole = this.filterRole
+      }
       axios
-        .get('/users', {
-          params: {
-            searchEmail: this.searchQuery,
-            filterRole: this.filterRole,
-            sortByCreateDate: this.sortOrder,
-            sortField: this.sortField,
-          },
+        .get('/users', { params })
+        .then((response) => {
+          this.users = response.data
         })
-        .then(response => {
-          this.users = response.data;
+        .catch((error) => {
+          console.error('There was an error fetching the users:', error)
         })
-        .catch(error => {
-          console.error('There was an error fetching the users:', error);
-        });
     },
     deleteUser(userId) {
-      if (confirm("Are you sure you want to delete this user?")) {
+      if (confirm('Are you sure you want to delete this user?')) {
         axios
           .delete(`/users/delete/${userId}`)
           .then(() => {
-            this.fetchUsers();
+            this.fetchUsers()
           })
-          .catch(error => {
-            console.error('There was an error deleting the user:', error);
-          });
+          .catch((error) => {
+            console.error('There was an error deleting the user:', error)
+          })
       }
-    }, 
-    navigateTo(routeName, queryParams = {}) {
-      this.$router.push({ name: routeName, query: queryParams });
     },
+    navigateTo(routeName, queryParams = {}) {
+      this.$router.push({ name: routeName, query: queryParams })
+    }
   },
   mounted() {
-    this.fetchUsers();
-  },
-};
+    this.fetchUsers()
+  }
+}
 </script>
 <style>
 @import 'users.scss';

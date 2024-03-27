a14fe15ff4b07bb1ddcb01ec9ae31fd2e33f2747
NickS721 <113018071+NickS721@users.noreply.github.com>
2024-03-27 11:52:04 +0100

    merge

--- src/stores/tickets.js
+++ src/stores/tickets.js
@@ -17,29 +17,24 @@ export const useTicketsStore = defineStore('tickets',  {
         const ticketDetails = await Promise.all(
           uniqueTicketIds.map(async (ticketId) => {
             const response = await axios.get(`/event/${ticketId}`);
-            return response.data; // Assuming the response contains ticket details
+            return response.data; 
           })
         );
 
-        // Flatten the array of ticket details and store them
         this.tickets = ticketDetails.flat();
       } catch (error) {
         console.error('Error fetching ticket details:', error);
       }
     },
     addTicket(ticketId) {
-      // Add the ticket ID to the tickets array in the store
       this.tickets.push(ticketId);
       localStorage.setItem('tickets', JSON.stringify(this.tickets));
     },
     removeTicket(ticketId) {
-      // Remove the ticket with the given ID from the tickets array in the store
       this.tickets = this.tickets.filter((ticket) => ticket !== ticketId);
-      // Update the localStorage with the updated tickets array
       localStorage.setItem('tickets', JSON.stringify(this.tickets));
     },
     getTicketById(ticketId) {
-      // Count the occurrences of the ticket with the given ID in the tickets array 
       return this.tickets.filter((ticket) => ticket === ticketId).length || 0;
     },
   },

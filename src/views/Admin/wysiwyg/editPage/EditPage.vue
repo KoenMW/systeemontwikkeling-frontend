74dcb7f2c172198af215e96334521719623be1a6
LukoGi <lukopecco@gmail.com>
2024-04-04 17:18:38 +0200

    Add authentication token to API requests

--- src/views/Admin/wysiwyg/editPage/EditPage.vue
+++ src/views/Admin/wysiwyg/editPage/EditPage.vue
@@ -57,6 +57,7 @@
 
 <script>
 import axios from '../../../../axios-auth';
+import { useAuthStore } from '../../../../stores/auth.js';
 
 export default {
   data() {
@@ -64,23 +65,29 @@ export default {
             pageData: {},
             infoText: [],
             links: [],
-            new: false,
+            new: true,
             isParent: false,
-            parentOptions: []
+            parentOptions: [],
+            token: null,
         }
   },
   mounted() {
+    const authStore = useAuthStore();
+    this.token = authStore.getJwt;
+
     const pageId = this.$route.params.id;
+    if (pageId !== 'new') {
     axios.get(`/pages/${pageId}`)
       .then(response => {
         this.pageData = response.data;
         this.pageData.infoText.forEach((info) => {
           this.infoText.push(info);
         })
+        this.new = false;
       }).catch(error => {
         console.error('There was an error fetching the content:', error);
-        this.new = true;
       });
+    }
 
     axios.get('/pages/links')
       .then(response => {
@@ -144,9 +151,14 @@ export default {
       });
     },
     savePage() {
-      // dit moet waarschijnlijk anders
       if (this.new) {
-        axios.post('/pages', this.pageData)
+        this.pageData.infoText = this.infoText;
+        console.log(this.pageData);
+        axios.post('/pages', this.pageData, {
+          headers: {
+            'Authorization': `Bearer ${this.token}`
+          }
+        })
           .then(response => {
             console.log(response);
           })
@@ -154,7 +166,13 @@ export default {
             console.log(error);
           });
       } else {
-      axios.put(`/pages/${this.pageData.id}`, this.pageData)
+        this.pageData.infoText = this.infoText;
+        console.log(this.pageData);
+      axios.put(`/pages/${this.pageData.id}`, this.pageData, {
+          headers: {
+            'Authorization': `Bearer ${this.token}`
+          }
+        })
         .then(response => {
           console.log(response);
         })
